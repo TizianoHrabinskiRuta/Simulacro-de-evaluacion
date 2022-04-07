13307b498b7260ed2320e7e34d38d3f5417ba293
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-04-07 15:04:15 -0300

    Done

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    //Crea un programa que permita ingrtesar por inspector dos valores enteros y repsonda si son iguales o no lo son.
+
+    public int Num1;
+    public int Num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Num1 == Num2) Debug.Log("Son iguales");
+        else Debug.Log("No son iguales");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+
+    //Crear las variables necesarias para ingresar por inspector el precio de 3 productos y un monto de dinero disponible. 
+    // Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuanto dinero sobra o falta. 
+
+    public int Prod1;
+    public int Prod2;
+    public int Prod3;
+
+    public float Dinero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Convert.ToSingle(Prod1 + Prod2 + Prod3) > Dinero)
+        {
+             float I =  Convert.ToSingle(Prod1 + Prod2 + Prod3) - Dinero;
+            Debug.Log("Faltan: $" + I.ToString());
+        }
+        else
+        {
+            float J = Dinero - Convert.ToSingle(Prod1 + Prod2 + Prod3) ;
+            Debug.Log("Sobra: $" + J.ToString());
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
